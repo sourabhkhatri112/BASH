0f8ed551e893502aa4faac144882c4f062180756
Sourabh Khatri <87218185+sourabhkhatri112@users.noreply.github.com>
2023-12-19 19:15:19 +0530

    Update Intro.sh
    Added Further loops . 
    How to call functions. 
    How to read inputs.

--- Intro.sh
+++ Intro.sh
@@ -15,6 +15,56 @@ if [[ "Hello" == "HELLO" ]]; then	# Use If-Else --> Pay attention to the spaces
 else
 	echo "Strings are not equal"
 fi
+# Output --> Strings are equal
+
+#-------------------------------------------------------------------------------------------------------------------------------
+
+for i in {1..5}; do		# Example For Loop
+	echo -n " Counting: $i"		# -n --> used to avoid new line
+done					# done used here to end it
+
+echo ""					# Used for the new Line here
+
+i=1
+while [[ $i -le 5 ]]; do
+	echo -n " While Loop : $i"	# -n --> used to avoid new line
+	i=$((i+1))		# Space should not be present here
+done
+
+echo ""					# Used for the new Line here
+
+i=1
+until [[ $i -gt 7 ]]; do 
+	echo -n " Do-While Loop : $i"	# -n --> used to avoid new line
+	i=$((i+1))						# Space should not be present here
+done
+
+
+
+#------------------------------------------------------------------------------------------------------------------------------
+echo ''					# Used for the new Line here
+
+# How to call a parameterised function and pass parameters
+my_function(){				
+	echo "Hello $1, $2 is your favourite color"
+}
+
+my_function "John" "Blue"			
+
+#------------------------------------------------------------------------------------------------------------------------------
+echo ''					# Used for the new Line here
+
+# How to read inputs
+echo "What is your name"
+read name
+
+echo "Hello $name"
+
+# "-p" prompts user to store input on same line.
+# User input is stored in "color" variable in same line
+# Echo then used afterwards to simply print the variable
+read -p "What is your favorite color? " color
+echo "Your favorite color is $color."
+
 /bin/bash				# The terminal stays open instead of force closing
 
-# Output --> Strings are equal
